be1e27eb24cec08be77e9255203cff2e96bc53db
theKBro <K.Broselge@gmx.net>
2015-09-10 12:17:36 +0200

    LoadProject on startup

--- car-management/car-management/App.xaml.cs
+++ car-management/car-management/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using car_management.Common;
 
 namespace car_management
 {
@@ -13,5 +14,14 @@ namespace car_management
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //TODO: show startup gui
+
+            DataManager dataManager = DataManager.Instance;
+            dataManager.LoadProject();
+
+            base.OnStartup(e);
+        }
     }
 }
