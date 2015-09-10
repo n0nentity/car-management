2c39009544792e0cdf4b883644fb43bc0cb2e92c
patdhlk <p@pichu.io>
2015-09-10 09:31:32 +0200

    added basic graph

--- car-management/car-management/ViewModel/MainViewModel.cs
+++ car-management/car-management/ViewModel/MainViewModel.cs
@@ -1,4 +1,6 @@
 using GalaSoft.MvvmLight;
+using OxyPlot;
+using OxyPlot.Series;
 
 namespace car_management.ViewModel
 {
@@ -16,6 +18,8 @@ namespace car_management.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
+        private PlotModel _graphModel;
+
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
@@ -24,10 +28,46 @@ namespace car_management.ViewModel
             if (IsInDesignMode)
             {
                 
+
             }
             else
             {
                 
+            }
+        }
+
+        /// <summary>
+        /// Gets the plot model.
+        /// </summary>
+        public PlotModel GraphModel
+        {
+            get
+            {
+                if (IsInDesignMode)
+                {
+                    // Create the plot model
+                    _graphModel = new PlotModel(){Title = "Verbrauchsdaten", Subtitle = "Autoname"};
+
+                    // Create two line series (markers are hidden by default)
+                    var series1 = new LineSeries { Title = "Series 1", MarkerType = MarkerType.Circle };
+                    series1.Points.Add(new DataPoint(0, 0));
+                    series1.Points.Add(new DataPoint(10, 18));
+                    series1.Points.Add(new DataPoint(20, 12));
+                    series1.Points.Add(new DataPoint(30, 8));
+                    series1.Points.Add(new DataPoint(40, 15));
+
+                    var series2 = new LineSeries { Title = "Series 2", MarkerType = MarkerType.Square };
+                    series2.Points.Add(new DataPoint(0, 4));
+                    series2.Points.Add(new DataPoint(10, 12));
+                    series2.Points.Add(new DataPoint(20, 16));
+                    series2.Points.Add(new DataPoint(30, 25));
+                    series2.Points.Add(new DataPoint(40, 5));
+
+                    // Add the series to the plot model
+                    _graphModel.Series.Add(series1);
+                    _graphModel.Series.Add(series2);
+                }
+                return _graphModel;
             }
         }
     }
